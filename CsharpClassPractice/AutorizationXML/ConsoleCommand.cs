44299ce1e6a6fe0c0f69e7751f7aa67f07ef61bd
mr.serhioru@mail.ru <mr.serhioru@mail.ru>
2022-10-17 21:16:07 +0300

    класс авторизация для викторины

---
+++ CsharpClassPractice/AutorizationXML/ConsoleCommand.cs
@@ -0,0 +1,50 @@
+﻿using AuthoriseXML;
+using System.Xml;
+using System.Xml.Linq;
+
+
+namespace AutorizationXML
+{
+    internal class ConsoleCommand
+    {
+
+        public static int AuthCount = 0;
+        public static void MustAuth()
+        {
+            Console.WriteLine("Авторизуйтесь! Заполните поля логина и пароля.");
+        }
+        public static User Auth()
+        {
+            Console.WriteLine("Введите логин:");
+            string login = Console.ReadLine();
+            Console.WriteLine("Введите пароль:");
+            string password = Console.ReadLine();
+            User user = new User(login, password);
+            return user;
+        }
+        public static void SucessAuth()
+        {
+            Console.WriteLine("Вы успешно авторизовались.");
+        }
+        public static void FailedAuth()
+        {
+            Console.WriteLine("Вы не смогли авторизоваться.");
+        }
+        public static string UserStart()
+        {
+            Console.WriteLine("Для авторизации наберите auth, для регистрации наберите register");
+            string answer = "";
+            switch (Console.ReadLine())
+            {
+                case "auth": answer = "auth"; break;
+                case "register": answer = "register"; break;
+                default: Console.WriteLine("Введенная вами команда не распознана."); UserStart(); break;
+            }
+            return answer;
+        }
+        public static void InformationAfterAuth()
+        {
+            Console.WriteLine("Возможные команды: \n quizlist");
+        }
+    }
+}
--- CsharpClassPractice/AutorizationXML/Program.cs
+++ CsharpClassPractice/AutorizationXML/Program.cs
@@ -1,77 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AuthoriseXML;
+using AutorizationXML;
 using System.Xml;
-using System.Xml.XPath;
+using System.Xml.Linq;
 
-namespace XmlAutorizer
+
+namespace AuthoriseXML
 {
     internal class Program
     {
-        static bool Authorisefailed = true;
+        static bool AuthoriseFailed = true;
         static void Main(string[] args)
         {
-            while(Authorisefailed)
-            {
-                ConsoleCommand.MustAuthorize();
-                Authorisefailed = Authorize;
-            }
+            do {
+                if (ConsoleCommand.UserStart() == "auth")
+                {
+                    while (AuthoriseFailed)
+                    {
+                        ConsoleCommand.MustAuth();
+                        AuthoriseFailed = Authorize.GetUserAuthorize(ConsoleCommand.Auth());
+                        if (AuthoriseFailed) ConsoleCommand.FailedAuth();
+                    }
+                    if (!AuthoriseFailed) ConsoleCommand.SucessAuth();
+                }
+                else
+                {
+                    string login;
+                    do
+                    {
+                        Console.WriteLine("Введите желаемый логин");
+                        login = Console.ReadLine();
+                    }
+                    while (Authorize.ExistUser(login));
+                    Console.WriteLine("Введите желаемый пароль");
+                    string password = Console.ReadLine();
+                    Console.WriteLine("Введите дату рождения в формате 2000.12.28");
+                    string[] date = Console.ReadLine().Split('.');
+                    DateTime dateOfBirth = new DateTime(Int32.Parse(date[0]), Int32.Parse(date[1]), Int32.Parse(date[2]));
+                    Authorize.SaveUserData(login, password, dateOfBirth);
+                }
+            } while (Authorize.UserInSystem == true);
         }
     }
-    class User
-    {
-        string _name;
-        string _password;
-        DateTime _birthday;
-        public string Login { get => _name; set => _name = value; }
-        public string Password { get => _password; set => _password = value; }
-        public DateTime DateOfBirth { get => _birthday; set => _birthday = value; }
-        public User(string name, string password, DateTime dateofbirth)
-        {
-            Login = name;
-            Password = password;
-            DateOfBirth = dateofbirth;
-        }
-        public User(string name, string password)
-        {
-            Login = name;
-            Password = password;
-            DateOfBirth = DateTime.Now;
-        }
-    }
-    class Authorize
-    {
-        static bool GetUserAuthorize(User name)
-        {
-            XmlDocument xdoc = new XmlDocument();
-            xdoc.Load("users.xml");
-
-            foreach(XmlElement element in xdoc.GetElementsByTagName("user"))
-            {
-                if(element.GetElementsByTagName("login")[0].InnerText)
-            }
-        }
-        static void SetUserData(string log, string pas, DateTime date)
-        {
-            
-        }
-    }
-    class ConsoleCommand
-    {
-        public static void MustAuthorize()
-        {
-            Console.WriteLine("Авторизуйся Живо!!!");
-
-        }
-        public static void Auth()
-        {
-            Console.WriteLine("Введите логин!!!");
-            string login = Console.ReadLine();
-            Console.WriteLine("Введите Пароль!!!");
-            string password = Console.ReadLine();
-        }
-    }
+           
 }
