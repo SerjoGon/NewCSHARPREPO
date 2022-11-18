5ff4d978cf4f77f90af01c67510892fd66425d7c
SergeyGoncharov <mr.serhioru@mail.ru>
2022-11-18 11:12:14 +0300

    Value from ini files to property

--- CsharpClassPractice/AttributePropertyDZ12/Program.cs
+++ CsharpClassPractice/AttributePropertyDZ12/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Xml.Linq;
 
 namespace AttributePropertyDZ12
 {
@@ -6,38 +7,42 @@ namespace AttributePropertyDZ12
     {
         static void Main(string[] args)
         {
-            Human hum = new Human();
+            Human hum = new Human(typeof(Human));
             Console.WriteLine(hum.ToString());
-
         }
     }
-    [AttributeUsage(AttributeTargets.Property)]
+    [AttributeUsage(AttributeTargets.All)]
     public class PropertyAttribute : Attribute
     {
-        public static string? _path { get; set; }
-        public PropertyAttribute(string path)
+        public string? _value_name { get; set; }
+        public string? _value_lastname { get; set; }
+        public string? _value_dofb { get; set; }
+        public PropertyAttribute() { }
+        public PropertyAttribute(string pathtoname,string pathtolastname,string pathtodofb)
         {
-            _path = path;
-
+            try
+            {
+            using StreamReader? readername = new StreamReader(pathtoname);
+            using StreamReader? readerlastname = new StreamReader(pathtolastname);
+            using StreamReader? readerdofb = new StreamReader(pathtodofb);
+            _value_name = readername.ReadToEnd();
+            _value_lastname = readerlastname.ReadToEnd();
+            _value_dofb = readerdofb.ReadToEnd();
+            }
+            catch(NullReferenceException ex)
+            {
+                throw new ArgumentException(ex.Message, nameof(pathtoname));
+            }
         }
     }
+     [Property("name.ini", "lastname.ini", "dateofbirth.ini")]
     class Human
     {
-        [Property("name.ini")]
-        public string Name { get 
-            {
-                return Name;
-            }
-            set 
-            {
-           
-                using (StreamReader reader = new StreamReader(PropertyAttribute._path))
-                {
-                    Name = reader.ReadToEnd();
-                }
-            }
-        }
-
+        PropertyAttribute? MyAttribute;
+        public string? Name { get; set; }
+        public string? LastName { get; set; }
+        public string? Dayofbirth{ get; set; }
+        /*
         [Property("lastname.ini")]
         public string LastName
         {
@@ -60,16 +65,29 @@ namespace AttributePropertyDZ12
             set
             {
                 using (StreamReader reader = new StreamReader(PropertyAttribute._path))
-                {
                     _dateofbirth =  Convert.ToDateTime(reader.ReadToEnd());
-                }
+            }
+        } */
+        public Human() { }
+        public Human(Type t)
+        {
+            try 
+            {
+            MyAttribute = (PropertyAttribute)Attribute.GetCustomAttribute(t, typeof(PropertyAttribute));
+            Name = MyAttribute._value_name;
+            LastName = MyAttribute._value_lastname;
+            Dayofbirth = MyAttribute._value_dofb;
+            }
+            catch(NullReferenceException ex)
+            {
+                throw new ArgumentException(ex.Message, nameof(MyAttribute));
             }
         }
-        public Human()
-        { }
+        
         public override string ToString()
         {
-            return base.ToString();
+            return "Значение взято из INI файлов " + Name + " " + LastName + " " + Dayofbirth + " ";
         }
+       
     }
 }
