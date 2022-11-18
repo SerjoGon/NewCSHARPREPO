bc2f7d3412d2b857c58b71effd6f57bb331169a5
mr.serhioru@mail.ru <mr.serhioru@mail.ru>
2022-11-18 20:10:43 +0300

    propertyattribute done

--- CsharpClassPractice/AttributePropertyDZ12/Program.cs
+++ CsharpClassPractice/AttributePropertyDZ12/Program.cs
@@ -1,93 +1,97 @@
 ﻿using System.IO;
 using System.Xml.Linq;
 
-namespace AttributePropertyDZ12
-{
-    internal class Program
+
+    using System.Reflection;
+    using System.Runtime.CompilerServices;
+
+    namespace ConsoleApp2
     {
-        static void Main(string[] args)
+        internal class Program
         {
-            Human hum = new Human(typeof(Human));
+            static void Main(string[] args)
+            {
+                Human hum = new Human(typeof(Human));
+                hum.GetAttribute(typeof(Human));
             Console.WriteLine(hum.ToString());
-        }
-    }
-    [AttributeUsage(AttributeTargets.All)]
-    public class PropertyAttribute : Attribute
-    {
-        public string? _value_name { get; set; }
-        public string? _value_lastname { get; set; }
-        public string? _value_dofb { get; set; }
-        public PropertyAttribute() { }
-        public PropertyAttribute(string pathtoname,string pathtolastname,string pathtodofb)
-        {
-            try
-            {
-            using StreamReader? readername = new StreamReader(pathtoname);
-            using StreamReader? readerlastname = new StreamReader(pathtolastname);
-            using StreamReader? readerdofb = new StreamReader(pathtodofb);
-            _value_name = readername.ReadToEnd();
-            _value_lastname = readerlastname.ReadToEnd();
-            _value_dofb = readerdofb.ReadToEnd();
-            }
-            catch(NullReferenceException ex)
-            {
-                throw new ArgumentException(ex.Message, nameof(pathtoname));
             }
         }
-    }
-     [Property("name.ini", "lastname.ini", "dateofbirth.ini")]
-    class Human
-    {
-        PropertyAttribute? MyAttribute;
-        public string? Name { get; set; }
-        public string? LastName { get; set; }
-        public string? Dayofbirth{ get; set; }
-        /*
-        [Property("lastname.ini")]
-        public string LastName
+        [AttributeUsage(AttributeTargets.All)]
+        public class PropertyAttribute : Attribute
         {
-            get
+            public string? _value_name { get; set; }
+            public string? _value_lastname { get; set; }
+            public string? _value_dofb { get; set; }
+            public PropertyAttribute() { }
+            public PropertyAttribute(string pathtoname, string pathtolastname, string pathtodofb)
             {
-                return LastName;
-            }
-            set
-            {
-                using (StreamReader reader = new StreamReader(PropertyAttribute._path))
+                try
                 {
-                    LastName = reader.ReadToEnd();
+                    using StreamReader? readername = new StreamReader(pathtoname);
+                    using StreamReader? readerlastname = new StreamReader(pathtolastname);
+                    using StreamReader? readerdofb = new StreamReader(pathtodofb);
+                    _value_name = readername.ReadToEnd();
+                    _value_lastname = readerlastname.ReadToEnd();
+                    _value_dofb = readerdofb.ReadToEnd();
+                }
+                catch (NullReferenceException ex)
+                {
+                    throw new ArgumentException(ex.Message, nameof(pathtoname));
                 }
             }
         }
-            [Property("dateofbirth.ini")]
-        public DateTime _dateofbirth
+        [Property("name.ini", "lastname.ini", "dateofbirth.ini")]
+        class Human
         {
-            get { return _dateofbirth; }
-            set
+            PropertyAttribute? MyAttribute;
+            [Property("name.ini", "lastname.ini", "dateofbirth.ini")]
+            public string? Name { get; set; }
+            [Property("name.ini", "lastname.ini", "dateofbirth.ini")]
+            public string? LastName { get; set; }
+            [Property("name.ini", "lastname.ini", "dateofbirth.ini")]
+            public string? Dayofbirth { get; set; }
+
+            public void GetAttribute(Type t)
             {
-                using (StreamReader reader = new StreamReader(PropertyAttribute._path))
-                    _dateofbirth =  Convert.ToDateTime(reader.ReadToEnd());
+                PropertyAttribute att;
+
+                // Get the class-level attributes.
+                // Put the instance of the attribute on the class level in the att object.
+                att = (PropertyAttribute)Attribute.GetCustomAttribute(t, typeof(PropertyAttribute));
+                MemberInfo[] MyMemberInfo = t.GetProperties();
+                for (int i = 0; i < MyMemberInfo.Length; i++)
+                {
+                    att = (PropertyAttribute)Attribute.GetCustomAttribute(MyMemberInfo[i], typeof(PropertyAttribute));
+                    if (att == null)
+                    {
+                        Console.WriteLine("No attribute in member function {0}.\n", MyMemberInfo[i].ToString());
+                    }
+                    
+                }
             }
-        } */
-        public Human() { }
-        public Human(Type t)
-        {
-            try 
+            public Human() { }
+            public Human(Type t)
             {
-            MyAttribute = (PropertyAttribute)Attribute.GetCustomAttribute(t, typeof(PropertyAttribute));
-            Name = MyAttribute._value_name;
-            LastName = MyAttribute._value_lastname;
-            Dayofbirth = MyAttribute._value_dofb;
+                try
+                {
+                    MyAttribute = (PropertyAttribute)Attribute.GetCustomAttribute(t, typeof(PropertyAttribute));
+                    if (MyAttribute != null)
+                    {
+                        Name = MyAttribute._value_name;
+                        LastName = MyAttribute._value_lastname;
+                        Dayofbirth = MyAttribute._value_dofb;
+                    }
+                }
+                catch (NullReferenceException ex)
+                {
+                    throw new ArgumentException(ex.Message, nameof(MyAttribute));
+                }
             }
-            catch(NullReferenceException ex)
+
+            public override string ToString()
             {
-                throw new ArgumentException(ex.Message, nameof(MyAttribute));
+                return "Значение взято из INI файлов " + Name + " " + LastName + " " + Dayofbirth + " ";
             }
+
         }
-        
-        public override string ToString()
-        {
-            return "Значение взято из INI файлов " + Name + " " + LastName + " " + Dayofbirth + " ";
-        }
-       
     }
-}
